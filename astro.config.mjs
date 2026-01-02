30e51370b408b240e4471bbcceaeeaa78cb72258
Eray Ates <eates23@gmail.com>
2026-01-02 14:32:46 +0100

    fix: remove relative lib
    Signed-off-by: Eray Ates <eates23@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 import sitemap from "@astrojs/sitemap";
 
-import relativeLinks from "astro-relative-links";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://1.tools",
-  integrations: [svelte(), sitemap(), relativeLinks()],
+  integrations: [svelte(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
